fc9f9f5cc4bb3a07cab79a7facab2402306a9feb
Varadarajan M <varad2k12@gmail.com>
2022-10-27 01:42:49 +0530

    shared projects : implemented read only control for shared users.

--- kanban-app/src/tasks/TaskCard.jsx
+++ kanban-app/src/tasks/TaskCard.jsx
@@ -1,45 +1,34 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './TaskCard.scss';
 import Icon from '../common/Icon';
+import { useProjectData } from '../hooks';
 
-const TaskCard = ({
-  taskItem,
-  onEditIconClick,
-  onDeleteIconClick,
-  isEditing,
-  changeHandler,
-  onEditSubmit,
-  style,
-}) => {
-  return (
-    <div className='task-card' style={{ ...style }}>
-      {isEditing ? (
-        <div className='edit__task'>
-          <input onChange={changeHandler} defaultValue={taskItem} />
-          <Icon
-            className='saveChanges__icon'
-            type='Done'
-            onClick={onEditSubmit}
-            tooltip={'Save Changes'}
-          />
-        </div>
-      ) : (
-        <span className='task__item'>{taskItem}</span>
-      )}
-      <Icon
-        type={isEditing ? 'Close' : 'Edit'}
-        className={'edit__icon'}
-        onClick={onEditIconClick}
-        tooltip={isEditing ? 'Discard' : 'Edit'}
-      />
-      <Icon
-        type='Delete'
-        className={'delete__icon'}
-        onClick={onDeleteIconClick}
-        tooltip={'Delete Task'}
-      />
-    </div>
-  );
+const TaskCard = ({ taskItem, onEditIconClick, onDeleteIconClick, isEditing, changeHandler, onEditSubmit, style }) => {
+	const { isProjectShared } = useProjectData();
+
+	return (
+		<div className='task-card' style={{ ...style }}>
+			{isEditing && !isProjectShared ? (
+				<div className='edit__task'>
+					<input onChange={changeHandler} defaultValue={taskItem} />
+					<Icon className='saveChanges__icon' type='Done' onClick={onEditSubmit} tooltip={'Save Changes'} />
+				</div>
+			) : (
+				<span className='task__item'>{taskItem}</span>
+			)}
+			{!isProjectShared && (
+				<Fragment>
+					<Icon
+						type={isEditing ? 'Close' : 'Edit'}
+						className={'edit__icon'}
+						onClick={onEditIconClick}
+						tooltip={isEditing ? 'Discard' : 'Edit'}
+					/>
+					<Icon type='Delete' className={'delete__icon'} onClick={onDeleteIconClick} tooltip={'Delete Task'} />
+				</Fragment>
+			)}
+		</div>
+	);
 };
 
 export default TaskCard;
