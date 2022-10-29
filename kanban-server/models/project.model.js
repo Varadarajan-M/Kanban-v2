3cc07ad0f7ebeba58b5d4319b11263b098236ce2
hemanthsharma13 <71222708+hemanthsharma13@users.noreply.github.com>
2022-10-30 00:13:06 +0530

    V2 schema (#9)
    * Implemented SharedProjects Schema
    
    * Test push
    
    * Fetch users API
    
    Co-authored-by: Ajay Srinivas A <ajaysrinivas333@gmail.com>
    Co-authored-by: hemanth <hemanthsharma1@github.com>

--- kanban-server/models/project.model.js
+++ kanban-server/models/project.model.js
@@ -1,18 +1,27 @@
 const mongoose = require('mongoose');
-
+const ProjectType = {
+  SHARED: 'shared',
+  OWN: 'own',
+};
 const ProjectSchema = new mongoose.Schema(
-	{
-		name: {
-			type: String,
-			default: 'Untitled project',
-		},
-		userId: {
-			type: mongoose.Types.ObjectId,
-			required: true,
-			ref: 'User',
-		},
-	},
-	{ timestamps: true },
+  {
+    name: {
+      type: String,
+      default: 'Untitled project',
+    },
+    userId: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    projectType: {
+      type: String,
+      enum: [ProjectType.SHARED, ProjectType.OWN],
+      required: true,
+      default: ProjectType.OWN,
+    },
+  },
+  { timestamps: true }
 );
 
 const Project = mongoose.model('Project', ProjectSchema);
